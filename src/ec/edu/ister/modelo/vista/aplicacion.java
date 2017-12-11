3d7ed3cd338ad091ac618e6dd36d00543d9078ba
DAVID <DAVID@DESKTOP-A17MQ8I>
2017-12-11 18:24:10 -0500

    ULTIMO DEBER ESTRUCTURA DE DATOS 11122017

---
+++ src/ec/edu/ister/modelo/vista/aplicacion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ister.modelo.vista;
+
+import ec.edu.ister.modelo.codigo;
+
+/**
+ *
+ * @author DAVID
+ */
+public class aplicacion {
+    public static void main(String[] args) {
+        codigo.menu();
+        codigo.llenararreglo();
+        codigo.Imprimir();
+        codigo.maximo();
+        codigo.ordenar();
+        
+}
+    }
